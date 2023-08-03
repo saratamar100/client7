590b8045e334a0a2a8d7a45898176c227a1190ad
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-08-03 16:46:17 +0300

    play sound

--- src/comps/Card.js
+++ src/comps/Card.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import OK1Audio from "../audio/OK1.mp3"
 
 function Card({ item }) {
   const handleAddToCart = (e) => {
     e.preventDefault();
+    playAudio(OK1Audio);
   };
   const handleAddToLove = (e) => {
     e.preventDefault();
+    playAudio(OK1Audio);
   };
+  function playAudio(url) {
+    new Audio(url).play();
+  }
   return (
     <div className="item" key={item.id}>
-      <Link to={"/item/" + item.id} >
+      <Link to={"/item/" + item.id}>
         <img className="img" src={item.img} alt="" />
         <div className="description">
           <p>{item.description}</p>
